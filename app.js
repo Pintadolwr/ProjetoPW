a4e61f106dceb7a71bb8c3f0d4e7be34da387bab
André Paiva <72794356+Paivex51@users.noreply.github.com>
2023-01-17 09:04:47 +0000

    authentication start

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 "use strict";
 const express = require("express");
+const express = require("express-session");
 const options = require("./config/options.json");
 /** @todo Completar */
+const autenticacaoRoutes = require("./routes/autenticacaoRoutes");
 const empresasRoutes = require("./routes/empresasRoutes");
 const portfoliosRoutes = require("./routes/portfoliosRoutes");
 const bodyParser = require("body-parser");
+const { application } = require("express");
 const app = express();
 
 app.use(bodyParser.json());
@@ -15,6 +18,9 @@ app.use(express.static("www"));
 app.get("/empresa", empresasRoutes.getEmpresas);
 
 // Countries
+
+//autenticacao
+app.post("/auth", autenticacaoRoutes.autenticate())
 
 app.listen(options.server.port, function () {
     console.log("Server running at port:" + options.server.port);
